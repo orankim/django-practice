2fedac90dccceef33737a0a45ceb4d7a5ee74fe9
orankim11 <orankim11@gmail.com>
2023-06-13 22:43:13 +0900

    blog post

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rx%os_zxd%143jb*ghnij@9ow46m#!)o=&6kq8$mn0v1lyy4us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
